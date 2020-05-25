faf23039470f98c3a8b94e33f1e548d5366d82e8
Roger Terres <31545931+rogerterres@users.noreply.github.com>
2020-05-25 17:47:32 -0300

    StatementsWhiteSpaceAndIndenting

---
+++ StatementsWhiteSpaceAndIndenting/src/com/javamasterclass/Main.java
@@ -0,0 +1,26 @@
+package com.javamasterclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50; // Statement : int myVariable = 50; // Expression : myVariable = 50
+        if (myVariable == 50) {
+            System.out.println("Printed");
+        }
+
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+                " another" +
+                "still more.");
+
+        int anotherVariable = 50;myVariable--;System.out.println("This is another one");
+        int                             myNewVariable
+                =
+                    60
+                        ;
+    }
+}
