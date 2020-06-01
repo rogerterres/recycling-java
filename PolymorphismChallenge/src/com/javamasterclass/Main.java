6370c99eab9e44a48fa71f16534c96075d83abe7
Roger Terres <31545931+rogerterres@users.noreply.github.com>
2020-06-01 12:15:51 -0400

    PolymorphismChallenge

---
+++ PolymorphismChallenge/src/com/javamasterclass/Main.java
@@ -0,0 +1,135 @@
+package com.javamasterclass;
+
+class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+    public String startEngine() {
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate() {
+        return "Car -> accelerate()";
+    }
+
+    public String brake() {
+        return "Car -> brake()";
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Mitsubishi extends Car {
+
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
+}
+
+class Holden extends Car {
+
+    public Holden(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() + " -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " -> brake()";
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //
+        // We are going to go back to the car analogy.
+        // Create a base class called Car
+        // It should have a few fields that would be appropriate for a generic car class
+        // Engine, cylinders, wheels, etc.
+        // Constructor should initialize cylinders (number of) and name, and set wheels to 4
+        // and engine to true. Cylinders and names would be passed parameters.
+        //
+        // Create appropriate getters
+        // Create some methods like startEngine, accelerate, and brake
+        // Show a message for each in the base class
+        // Override the appropriate methods to demonstrate polymorphism in use.
+        // Put all classes in one Java file (this one)
+        //
+
+        Car car = new Car(8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Car ford = new Car(6, "Ford Falcon") {
+            @Override
+            public String startEngine() {
+                return "Ford -> startEngine()";
+            }
+
+            @Override
+            public String accelerate() {
+                return "Ford -> accelerate()";
+            }
+
+            @Override
+            public String brake() {
+                return "Ford -> brake()";
+            }
+        };
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Holden holden = new Holden(6, "Holden Commodore");
+        System.out.println(holden.startEngine());
+        System.out.println(holden.accelerate());
+        System.out.println(holden.brake());
+    }
+}
